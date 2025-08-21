fd89b198dfd8b475950c710459199caef2fe7b4a
Denys <80990259+denysdzhyma@users.noreply.github.com>
2025-08-21 12:22:52 -0400

    Added NavigationLink
    Added NavigationLink inside Text()

--- JournalApp/ContentView.swift
+++ JournalApp/ContentView.swift
@@ -39,14 +39,22 @@ struct JournalDetailView: View {
 struct ContentView: View {
     
     @State private var journals: [Journal] = [
-        Journal(title: "Work Log", entries: []),
+        Journal(title: "Work Log", entries: [
+            JournalEntry(title: "Met with team", text: "Discussed the new project timeline.", date: Date())
+        ]),
         Journal(title: "Personal Thoughts", entries: []),
-        Journal(title: "SwiftUI Journey", entries: [])
+        Journal(title: "SwiftUI Journey", entries: [
+            JournalEntry(title: "Learned about Lists", text: "Built a To-Do app today!", date: Date()),
+            JournalEntry(title: "Started Journal App", text: "Learning about multiple data models.", date: Date())
+        ])
     ]
     var body: some View {
         NavigationView {
             List(journals) { journal in
-                Text(journal.title)
+                
+                NavigationLink(destination: JournalDetailView(journal: journal)) {
+                    Text(journal.title)
+                }
             }.navigationTitle("My Journals")
         }
     }
